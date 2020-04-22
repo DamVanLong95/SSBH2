d081c7f77416dbe793defbfcec2cfa169a2e03e2
DamVanLong95 <longdam95@gmail.com>
2020-04-22 10:45:53 +0700

    change sidebar in admin

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -10,11 +10,6 @@
                         <li class="sidebar-item"><a href="{{route('posts.index')}}" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu"> Danh sach </span></a></li>
                     </ul>
                 </li>
-                <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="" aria-expanded="false"><i class="mdi mdi-receipt"></i><span class="hide-menu">Product </span></a>
-                    <ul aria-expanded="false" class="collapse  first-level">
-                        <li class="sidebar-item"><a href="{{route('product.index')}}" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu">Product(BH)</span></a></li>
-                    </ul>
-                </li>
                 <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="" aria-expanded="false"><i class="mdi mdi-receipt"></i><span class="hide-menu">Company </span></a>
                     <ul aria-expanded="false" class="collapse  first-level">
                         <li class="sidebar-item"><a href="{{route('excel.company')}}" class="sidebar-link"><i class="mdi mdi-emoticon-cool"></i><span class="hide-menu">Company Table </span></a></li>
@@ -36,14 +31,11 @@
                     <ul aria-expanded="false" class="collapse  first-level">
                         <li class="sidebar-item"><a href="{{route('indexScope.import')}}" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu">Scope(BH)</span></a></li>
                         <li class="sidebar-item"><a href="{{route('product_longevity.index')}}" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu">Product(BH)</span></a></li>
-                        <!-- <li class="sidebar-item"><a href="" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu">Cost(BH)</span></a></li>
-                        <li class="sidebar-item"><a href="" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu">Benifits(BH)</span></a></li>
-                        <li class="sidebar-item"><a href="" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu">Benifits more</span></a></li>
-                        <li class="sidebar-item"><a href="" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu">Insurance (loại trừ bảo hiểm)</span></a></li> -->
                     </ul>
                 </li>
                 <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="" aria-expanded="false"><i class="mdi mdi-face"></i><span class="hide-menu">Health </span></a>
                     <ul aria-expanded="false" class="collapse  first-level">
+                    <li class="sidebar-item"><a href="{{route('product.index')}}" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu">Product(BH)</span></a></li>
                         <li class="sidebar-item"><a href="{{route('indexHealth.import')}}" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu">Health</span></a></li>
                         <li class="sidebar-item"><a href="{{route('indexProgram.import')}}" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu">Program</span></a></li>
                         <li class="sidebar-item"><a href="{{route('indexHospital.import')}}" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu">Hospital</span></a></li>
