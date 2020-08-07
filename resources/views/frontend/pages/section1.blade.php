c08eae6c985fbe127cd1466fd5a2d4af7ced65ac
Hunghus <lehung1321@gmail.com>
2020-08-07 17:50:27 +0700

    hunglv change url backgroud

--- resources/views/frontend/pages/section1.blade.php
+++ resources/views/frontend/pages/section1.blade.php
@@ -1,19 +1,19 @@
 
 <div class="swiper-container swiper-banner">
   <div class="swiper-wrapper">
-    
+
     <div class="swiper-slide">
         <div class="overlay-bg">
-        <div class="slide-inner" style="background-image:url(./../assets/images/home/banner1.jpg)"></div>
+        <div class="slide-inner" style="background-image:url({{ url('/') }}/assets/images/home/banner1.jpg?{{ config('custom.version') }})"></div>
         </div>
     </div>
     <div class="swiper-slide">
         <div class="overlay-bg"></div>
-        <div class="slide-inner" style="background-image:url(./../assets/images/home/banner2.jpg)"></div>
+        <div class="slide-inner" style="background-image:url({{ url('/') }}/assets/images/home/banner2.jpg?{{ config('custom.version') }})"></div>
     </div>
     <div class="swiper-slide">
         <div class="overlay-bg"></div>
-        <div class="slide-inner" style="background-image:url(./../assets/images/home/banner3.jpg)"></div>
+        <div class="slide-inner" style="background-image:url({{ url('/') }}/assets/images/home/banner3.jpg?{{ config('custom.version') }})"></div>
     </div>
   </div>
 </div>
