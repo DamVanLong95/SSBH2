a788aa69dda3be1afbd078b913f8044b116956c8
Hunghus <lehung1321@gmail.com>
2020-08-07 17:51:03 +0700

    add 404 redirect home

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,24 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        if($this->isHttpException($exception))
+        {
+            switch (intval($exception->getStatusCode())) {
+                // not found
+                case 500:
+                case 404:
+                    return \Response::view('frontend.home');
+                    break;
+                // internal error
+
+                default:
+                    return $this->renderHttpException($exception);
+                    break;
+            }
+        }
+        else
+        {
+            return parent::render($request, $exception);
+        }
     }
 }
