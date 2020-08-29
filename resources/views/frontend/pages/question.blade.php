5ff8ed7e8a8b5445eec6b1600b91e4e7a31abd02
anhnguyenthi <anhnt94.hanu@gmail.com>
2020-08-29 10:56:01 +0700

    add question sign

--- resources/views/frontend/pages/question.blade.php
+++ resources/views/frontend/pages/question.blade.php
@@ -93,30 +93,37 @@
                                     <div class="alpha-ctn">
                                         <div class="alpha-left" id="content">
                                             @foreach($libraries as $group_libra)
-                                                @foreach($group_libra as $value)
-                                                    <div class="alpha-index">
-                                                        <div class="alpha-left">
-                                                            <a href="#C" class="{{$value['content']!='' ? '' : 'disable' }}">{{$value['character']}}</a>
-                                                        </div>
-                                                        <div class="index-content">
-                                                            <div class="panel panel-default">
-                                                                <div class="panel-heading">
-                                                                    <h4 class="panel-title">
-                                                                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse{{$value['id']}}" class="collapsed" aria-expanded="false">
-                                                                        {{$value['terms']}}</a>
-                                                                    </h4>
+                                                <div class="sign-ctn">
+                                                    <div class="sign-index">
+                                                    <h3>A</h3>
+                                                    </div>
+                                                    <div class="sign-content">
+                                                        @foreach($group_libra as $value)
+                                                            <div class="alpha-index">
+                                                                <div class="alpha-left">
+                                                                    <a href="javascript:void(0);" class="{{$value['content']!='' ? '' : 'disable' }}">{{$value['character']}}</a>
                                                                 </div>
-                                                                <div id="collapse{{$value['id']}}" class="panel-collapse collapse" aria-expanded="false">
-                                                                    <div class="panel-body">
-                                                                        <p>
-                                                                            {{strip_tags($value['content'])}}
-                                                                        </p>
+                                                                <div class="index-content">
+                                                                    <div class="panel panel-default">
+                                                                        <div class="panel-heading">
+                                                                            <h4 class="panel-title">
+                                                                                <a data-toggle="collapse" data-parent="#accordion" href="#collapse{{$value['id']}}" class="collapsed" aria-expanded="false">
+                                                                                {{$value['terms']}}</a>
+                                                                            </h4>
+                                                                        </div>
+                                                                        <div id="collapse{{$value['id']}}" class="panel-collapse collapse" aria-expanded="false">
+                                                                            <div class="panel-body">
+                                                                                <p>
+                                                                                    {{strip_tags($value['content'])}}
+                                                                                </p>
+                                                                            </div>
+                                                                        </div>
                                                                     </div>
                                                                 </div>
                                                             </div>
-                                                        </div>
+                                                        @endforeach
                                                     </div>
-                                                @endforeach
+                                                </div>
                                             @endforeach
                                         </div>
                                     </div>
